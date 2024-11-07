4a2d4ecb25454381d2d4e6bca37e4d87dcf3b3fc
spielgrund <138134622+spielgrund@users.noreply.github.com>
2024-11-07 16:57:27 +0100

    second

--- src/clojureprojects/protein_translation.clj
+++ src/clojureprojects/protein_translation.clj
@@ -1,3 +1,25 @@
 (ns clojureprojects.protein-translation)
 
-hallo
+(defn translate-codon [codon]
+  (case codon
+    ("AUG") "Methionine"
+    ("UUU" "UUC") "Phenylalanine"
+    ("UUA" "UUG") "Leucine"
+    ("UCU" "UCC" "UCA" "UCG") "Serine"
+    ("UAU" "UAC") "Tyrosine"
+    ("UGU" "UGC") "Cysteine"
+    ("UGG") "Tryptophan"
+    ("UAA" "UAG" "UGA") "STOP"
+    ""))
+
+(defn translate-rna [s]
+  (first
+    (split-with #(not= % "STOP")
+                (map translate-codon
+                     (map #(apply str %) (partition 3  s)))))
+  )
+
+
+(defn -main []
+  (println (translate-codon "UGA"))
+  (println (translate-rna "AUGUUUUAAUCUA")))
--- src/clojureprojects/tests.clj
+++ src/clojureprojects/tests.clj
@@ -1,1 +1,5 @@
 (ns clojureprojects.tests)
+
+(defn -main []
+  (println (/ (* 3 (+ 1 2)) 4)))
+;; 9/4
